882a0d75c708e5fb259a0b9fc82854d658996680
lassekongo83 <lassekongo83@users.noreply.github.com>
2022-02-05 09:57:25 +0100

    Don't symlink the dark theme

--- gtk/src/adw-gtk3-dark/gtk-3.0/install-dark-theme.sh
+++ gtk/src/adw-gtk3-dark/gtk-3.0/install-dark-theme.sh
@@ -7,6 +7,7 @@ project_name="$2"
 install -m755 -d "${theme_dir}"
 for ver in gtk-3.0; do
   install -m755 -d "${theme_dir}/${ver}"
-  ln -sf "../../${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk.css"
-  ln -sf "../../${project_name}/${ver}/assets" "${theme_dir}/${ver}/assets"
+  #ln -sf "../../${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk.css"
+  #ln -sf "../../${project_name}/${ver}/assets" "${theme_dir}/${ver}/assets"
+  cp -r "gtk/src/${project_name}/${ver}/gtk-dark.css" "${theme_dir}/${ver}/gtk.css"
 done
