d2bb0323f448017717c2837518c5e7e70e8a581b
lassekongo83 <lassekongo83@users.noreply.github.com>
2023-03-02 07:56:49 +0100

    Update script

--- gitpush.sh
+++ gitpush.sh
@@ -6,6 +6,6 @@ read -r -p "Commit message: "  COMMIT_MESSAGE
 COMMIT=$(git commit -m "$COMMIT_MESSAGE");
 PUSH=$(git push -u origin main);
 
-echo $ADDFILES
-echo $COMMIT
-echo $PUSH
+echo "$ADDFILES"
+echo "$COMMIT"
+echo "$PUSH"
